909552acd554f0c919a01074d1d6543fc7056d71
Andreas Atteneder <andreas.atteneder@gmail.com>
2021-09-06 18:01:29 +0200

    fix: GLTFast.Test package got restructured. Adjusting to name changes

--- unity_performance/main.py
+++ unity_performance/main.py
@@ -4,6 +4,13 @@ from typing import List
 from pathlib import Path
 
 
+def remove_test_prefix(name: str):
+    result = name
+    result = result.removeprefix('GLTFast.Tests.')
+    result = result.removeprefix('GLTFTest.')
+    return result
+
+
 @dataclass_json
 @dataclass
 class SampleGroup:
@@ -39,7 +46,8 @@ class Report:
 
     def get_result_by_name(self, name):
         for r in self.Results:
-            if r.Name == name:
+            ref_name = remove_test_prefix(r.Name)
+            if ref_name == name:
                 return r
         return None
 
@@ -77,36 +85,62 @@ def create_load_time_report(
         output_file.write('\n')
 
         for ref_result in ref.Results:
-            results = [report.get_result_by_name(
-                ref_result.Name) for report in reports]
+            ref_result_name = remove_test_prefix(ref_result.Name)
+            results = [report.get_result_by_name(ref_result_name
+                                                 ) for report in reports]
 
-            if not ref_result.Name.startswith(test_prefix):
+            if not remove_test_prefix(ref_result.Name).startswith(test_prefix):
                 continue
 
             test_name = ref_result.Name[len(test_prefix):].lstrip('.')
-            output_file.write(test_name)
+            output_row = test_name
 
             for ref_grp in ref_result.SampleGroups:
                 if ref_grp.Name == group_name:
+
+                    if None in results:
+                        output_row = None
+                        continue
+
                     grps = [result.get_sample_group_by_name(
                         ref_grp.Name) for result in results]
+
+                    if None in grps:
+                        output_row = None
+                        continue
+
                     for grp in grps:
                         if all_stats:
-                            output_file.write(
-                                f';{grp.Min};{grp.Max};{grp.Median};{grp.Average}')
+                            output_row += f';{grp.Min};{grp.Max};{grp.Median};{grp.Average}'
                         else:
-                            output_file.write(f';{grp.Average}')
-            output_file.write('\n')
+                            output_row += f';{grp.Average}'
+
+            if output_row:
+                output_file.write(output_row)
+                output_file.write('\n')
 
 
 def main():
     report_paths = [
-        'input_data/mac_intel/2.5.1/PerformanceTestResults.json',
-        'input_data/mac_intel/async3_json/PerformanceTestResults.json',
+        # 'input_data/mac_intel/2.6.0/PerformanceTestResults.json',
+        # 'input_data/mac_intel/async3_json/PerformanceTestResults.json',
         # 'input_data/mac_intel/async4_base64/PerformanceTestResults.json',
         # 'input_data/mac_intel/async5_non_readable/PerformanceTestResults.json',
-        'input_data/mac_intel/async6_testfix/PerformanceTestResults.json',
+        # 'input_data/mac_intel/async6_testfix/PerformanceTestResults.json',
         # 'input_data/mac_intel/3.0.0/PerformanceTestResults.json',
+        # 'input_data/mac_intel/3.0.0-1/PerformanceTestResults.json',
+        # 'input_data/mac_intel/3.0.2/PerformanceTestResults.json',
+        # 'input_data/mac_intel/3.1.0/PerformanceTestResults.json',
+        # 'input_data/mac_intel/4.2.0/PerformanceTestResults.json',
+        'input_data/mac_intel/4.2.1/PerformanceTestResults.json',
+        'input_data/mac_intel/4.3.0_burst/PerformanceTestResults.json',
+
+        # 'input_data/mac_m1/2.6.0_rosetta/PerformanceTestResults.json',
+        # 'input_data/mac_m1/2.6.0/PerformanceTestResults.json',
+        # 'input_data/mac_m1/3.0.0/PerformanceTestResults.json',
+
+        # 'input_data/win/2.6.0/PerformanceTestResults.json',
+        # 'input_data/win/3.0.0/PerformanceTestResults.json',
     ]
     report_paths = [Path(x) for x in report_paths]
     report_names = [x.parent.name for x in report_paths]
@@ -116,7 +150,7 @@ def main():
         'output/smooth_load_times.csv',
         report_names,
         reports,
-        'GLTFast.Tests.SampleModelsTest.SmoothLoading',
+        'SampleModelsTest.SmoothLoading',
         'LoadTime'
     )
 
@@ -124,8 +158,8 @@ def main():
         'output/smooth_frame_times.csv',
         report_names,
         reports,
-        'GLTFast.Tests.SampleModelsTest.SmoothLoading',
-        'FrameTime',
+        'SampleModelsTest.SmoothLoading',
+        'Time',
         all_stats=True
     )
 
@@ -133,7 +167,7 @@ def main():
         'output/fast_load_times.csv',
         report_names,
         reports,
-        'GLTFast.Tests.SampleModelsTest.UninterruptedLoading',
+        'SampleModelsTest.UninterruptedLoading',
         'LoadTime'
     )
 
